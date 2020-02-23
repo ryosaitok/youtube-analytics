ca408dd3b2f908fb8e1d82b82930bb5aec89cc6e
Ryo Saito <ryosaito@RyonoMacBook-Pro.local>
2020-02-24 00:59:25 +0900

    add logged in / logged out

--- youtube-analytics-frontend/store/index.js
+++ youtube-analytics-frontend/store/index.js
@@ -54,6 +54,23 @@ export const actions = {
     this.$cookies.set('jwt_token', payload);
     commit('mutateToken', payload)
   },
+
+  async login({commit, dispatch}, payload) {
+    const res = await firebase.auth().signInWithEmailAndPassword(payload.email, payload.password);
+    const token = await res.user.getIdToken();
+    this.$cookies.set('jwt_token', token);
+    const refreshToken = res.user.refreshToken;
+    this.$cookies.set('refresh_token', refreshToken);
+    commit('mutateToken', token);
+    this.app.router.push('/')
+  },
+
+  async logout({commit}) {
+    await firebase.auth().signOut();
+    commit('mutateToken', null);
+    this.$cookies.remove('jwt_token');
+    this.app.router.push('/')
+  },
 };
 
 export const mutations = {
@@ -104,5 +121,9 @@ export const getters = {
 
   getSearchMeta(state) {
     return state.searchMeta
+  },
+
+  isLoggedIn(state) {
+    return !!state.token
   },
 };
